cce110c6022c7e2c61c92f38a31536f7dd5bc1d2
Jasmine-PI <140223394+Jasmine-PI@users.noreply.github.com>
2023-12-15 00:14:12 +1100

    Update acceleration and gravity

--- index.js
+++ index.js
@@ -5,26 +5,37 @@ const context = canvas.getContext("2d") // what API you want
 canvas.width = 1024
 canvas.height = 576
 
+const gravity = 0.5 // global
+
 class Player {
-    constructor() {
-        this.position = {
+    constructor(position) {
+        this.position = position
+        this.velocity = {
             x: 0,
-            y: 0
+            y: 1
         }
+        this.height = 100
     }
 
     draw() {
         context.fillStyle = "red"
-        context.fillRect(this.position.x, this.position.y, 100, 100)
+        context.fillRect(this.position.x, this.position.y, 100, this.height)
     }
 
     update() {
         this.draw()
-        this.position.y++
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y < canvas.height) { // fall keeps accelerating
+            this.velocity.y += gravity
+        }
+        else {
+            this.velocity.y = 0 // stops when bottom reached
+        }
     }
 }
 
-const player = new Player()
+const player = new Player({x:0, y:0})
 
 let y = 100
 
@@ -37,7 +48,6 @@ function animate() { // animation loop
 
     // player
     player.update()
-
 }
 
 animate() // calls loop

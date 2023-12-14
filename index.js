34c42f62caf1de1e7742cde04894d5c86ad7fb50
Jasmine-PI <140223394+Jasmine-PI@users.noreply.github.com>
2023-12-14 21:39:25 +1100

    Update code

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// import {Player} from "./Player.js"
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext("2d") // what API you want
 
-const canvas = document.querySelector('canvas') // we want the content from the canvas tag and above (?)
-const context = canvas.getContext('2d') // we want things in 2D, this allows us to do things/use 2D related methods (?)
-// tutorial used c and said it stood for 'context'
+// 16 : 9 ratio
+canvas.width = 1024
+canvas.height = 576
 
-
-canvas.width = 1024 // 64 * 16
-canvas.height = 576 // 64 * 9
-
-const player = new Player()
-
-// functions
-function animate() { // animation loop (?)
-    window.requestAnimationFrame(animate)
-    
-    // background
-    context.fillStyle = 'white'
-    context.fillRect(0, 0, canvas.width, canvas.height) // x-pos, y-pos, width, height
-
-    player.draw()
-    player.update()
-
-}
-
-animate()
+context.fillStyle = "white"
+context.fillRect(0, 0, canvas.width, canvas.height)

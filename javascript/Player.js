2b2102df059b18276ce93ac5857db862f585c944
Jasmine-PI <140223394+Jasmine-PI@users.noreply.github.com>
2023-12-16 20:45:28 +1100

    Add Collisions

--- javascript/Player.js
+++ javascript/Player.js
@@ -1,16 +1,18 @@
 class Player {
-    constructor(position) {
+    constructor({position}) {
         this.position = position
         this.velocity = {
             x: 0,
             y: 1
         }
+        this.width = 100
         this.height = 100
+        this.collisionBlocks = collisionBlocks
     }
 
     draw() {
         context.fillStyle = "red"
-        context.fillRect(this.position.x, this.position.y, 100, this.height)
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
     update() {
@@ -19,11 +21,31 @@ class Player {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity.y < canvas.height) { // fall keeps accelerating
-            this.velocity.y += gravity
-        }
-        else {
-            this.velocity.y = 0 // stops when bottom reached
+        this.applyGravity()
+
+        this.checkForVerticalCollisions()
+
+        // stops falling via canvas (not col-blocks)
+        // if (this.position.y + this.height + this.velocity.y < canvas.height) { // fall keeps accelerating
+        //     this.velocity.y += gravity
+        // }
+        // else {
+        //     this.velocity.y = 0 // stops when bottom reached
+        // }
+    }
+
+    applyGravity() {
+        this.position.y += this.velocity.y
+        this.velocity.y += gravity
+    }
+
+    checkForVerticalCollisions() {
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+
+            if (collision({object1:this, object2:collisionBlock})) {
+                console.log("successful collision")
+            }
         }
     }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -32,10 +32,36 @@ floorCollisions2D.forEach((row, y) => { // y: row index
         })
 })
 
+// platform collisions
+const platformCollisions2D = []
+for (let i = 0; i < platformCollisions.length; i+=36) {
+    platformCollisions2D.push(platformCollisions.slice(i, i+36))
+}
+
+const platformCollisionBlocks = []
+platformCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        platformCollisionBlocks.push(
+            new CollisionBlock({
+                position: {
+                    x: x * 16,
+                    y: y * 16
+                }
+            })
+        )
+    })
+})
+
 const gravity = 0.5 // global
 
 // player object
-const player = new Player({x:0, y:0})
+const player = new Player({
+    position: {
+        x: 500,
+        y: 0
+    },
+    collisionBlocks: collisionBlocks
+})
 
 // keys -- outside of Player
 const keys = {
@@ -43,6 +69,9 @@ const keys = {
         pressed: false
     },
     a: {
+        pressed: false
+    },
+    w: {
         pressed: false
     }
 }
@@ -69,9 +98,20 @@ function animate() { // animation loop
     context.scale(4, 4)
     context.translate(0, -background.image.height + scaledCanvas.height)
 
+    // floor
+    collisionBlocks.forEach((collisionBlocks) => {
+        collisionBlocks.update()
+    })
+
+    // platform
+    collisionBlocks.forEach((block) => {
+        block.update()
+    })
+
     // bg sprite
     background.update()
     context.restore() // scales only once
+
 
     // player
     player.update()
@@ -86,7 +126,7 @@ function animate() { // animation loop
     }
 }
 
-animate() // calls loop
+animate() // calls loop -- game loop?
 
 // event listeners
 
@@ -100,6 +140,12 @@ window.addEventListener('keydown', (event)=> {
             keys.a.pressed = true // keys pressed == keydown
             break
         case 'w':
+            keys.w.pressed = true
+            // if (player.velocity.y === 0) { // no double jump
+            //     player.velocity.y = -15
+            // }
+
+            // allows double jumps
             player.velocity.y = -15
             break
     }
@@ -112,6 +158,9 @@ window.addEventListener('keyup', (event)=> {
             break
         case 'a':
             keys.a.pressed = false
+            break
+        case 'w':
+            keys.w.pressed = false
             break
     }
 })
